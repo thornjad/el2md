42f3831b10990ad54c2bd49d9a59c8d9fcc4caf9
Jade Michael Thornton <jademichael@jmthornton.net>
2020-05-14 11:01:04 -0500

    update documentation

--- el2md.el
+++ el2md.el
@@ -1,60 +1,46 @@
 ;;; el2md.el --- Convert commentary section of elisp files to markdown. -*- lexical-binding:t -*-
 
-;; Copyright (c) 2020 Jade Michael Thornton
-;; Copyright (c) 2013-2014 Anders Lindgren
-
 ;; Author: Jade Michael Thornton
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://gitlab.com/thornjad/el2md
-
-;; This program is free software: you can redistribute it and/or modify it under
-;; the terms of the GNU General Public License as published by the Free Software
-;; Foundation, either version 3 of the License. This program is distributed in
-;; the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-;; implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
-;; the GNU General Public License for more details.
 
 ;;; Commentary:
 
-;; This package converts *Commentary* section in Emacs Lisp modules to text
-;; files in Markdown format, a format supporting headings, code blocks, basic
-;; text styles, bullet lists etc.
-;;
-;; The Markdown is used by many web sites as an alternative to plain texts. For
-;; example, it is used by sites like StackOverflow, GitLab and GitHub.
+;; This package converts the _Commentary_ section in an Elisp module to text
+;; files in the Markdown language. It supports most of Markdown, including some
+;; headings, code blocks, text styles, lists, etc.
 
 ;; What is converted:
 ;;
 ;; Everything between the `Commentary:' and `Code:' markers are included in the
-;; generated text. In addition, the title and *some* metadata are also included.
+;; generated text. In addition, the title and _some_ metadata is also included.
 
 ;; How to write comments:
 ;;
-;; The general rule of thumb is that the Emacs Lisp module should be written
-;; using plain text, as they always have been written.
-;;
-;; However, some things are recognized. A single line ending with a colon is
-;; considered a *heading*. If this line is at the start of a comment block, it
-;; is considered a main (level 2) heading. Otherwise it is considered a (level
-;; 3) subheading. Note that the line precedes a bullet list or code, it will not
-;; be treated as a subheading.
+;; The general rule of thumb is that the Elisp module should be written
+;; using plain text, as they always have been written. However, some things are
+;; recognized. A single line ending with a colon is considered a _heading_. If
+;; this line is at the start of a comment block, it is considered a main (level
+;; 2) heading. Otherwise it is considered a (level 3) subheading. Note that if
+;; the line precedes a bullet list or code, it will not be treated as a
+;; subheading.
 ;;
 ;; Use Markdown formatting:
 ;;
-;; It is possible to use markdown syntax in the text, like *this*, and **this**.
+;; It is possible to use markdown syntax in the text, like _this_, and **this**.
 ;;
 ;; Conventions:
 ;;
-;; The following conventions are used when converting elisp comments to
+;; The following conventions are used when converting Elisp comments to
 ;; Markdown:
 ;;
 ;; * Code blocks using either the Markdown convention by indenting the block
 ;;   with four extra spaces, or by starting a paragraph with a `('.
 ;;
-;; * In elisp comments, a reference to `code' (backquote - quote), they will be
-;;   converted to Markdown style (backquote - backquote).
+;; * In Elisp comments, a reference to `code' (backquote - quote) will be
+;;   converted to Markdown (backquote - backquote).
 ;;
-;; * In elisp comments, bullets in lists are typically separated by empty lines.
+;; * In Elisp comments, bullets in lists are typically separated by empty lines.
 ;;   In the converted text, the empty lines are removed, as required by
 ;;   Markdown.
 ;;
@@ -74,7 +60,7 @@
 ;;     ;;
 ;;     ;; Another paragraph.
 ;;     ;;
-;;     ;; This line is *not* as a subheading:
+;;     ;; This line is _not_ as a subheading:
 ;;     ;;
 ;;     ;; * A bullet in a list
 ;;     ;;
@@ -92,7 +78,7 @@
 ;;     M-x el2md-write-file RET name-of-file RET
 ;;
 ;; In sites like GitLab, if a file named readme.md exists in the root directory
-;; of an archive, it is displayed when viewing the archive. To generate a
+;; of an repository, it is displayed when viewing the repository. To generate a
 ;; readme.md file, in the same directory as the current buffer, use:
 ;;
 ;;     M-x el2md-write-readme RET
@@ -113,6 +99,26 @@
 ;; For example,
 ;;
 ;;     emacs -batch -l el2md.el my-file.el -f el2md-write-readme
+
+;; Known problems:
+;;
+;; - The end of the _Commentary_ section includes the first comment in the code
+;;   if there's no empty line before the _Code_ comment.
+
+;; License:
+;;
+;; `el2md' was originally forked from `el2markdown' by Anders Lindgren. It has
+;; since been improved, but carries the same license as the original work.
+;;
+;; Copyright (c) 2020 Jade Michael Thornton
+;; Copyright (c) 2013 Anders Lindgren
+;;
+;; This program is free software: you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free Software
+;; Foundation, version 3. This program is distributed in the hope that it will
+;; be useful, but without any warranty; without even the implied warranty of
+;; merchantability or fitness for a particular purpose. See the GNU General
+;; Public License for more details.
 
 ;;; Code:
 
